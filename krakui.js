8bcd9123467f546ff6378b6dbb66cda0585de90d
aivan <ivan.arandjelovic@gmail.com>
2015-01-22 20:11:29 +0100

    Keys provided

--- krakui.js
+++ krakui.js
@@ -1,19 +1,6 @@
 console.log("Krakui browser starting ...");
 
-// var KrakenClient = require('kraken-api');
-// var kraken = new KrakenClient('krakUiApiKey.api_key',
-// 'krakUiApiKey.api_secret');
+var KrakenClient = require('kraken-api');
+var kraken = new KrakenClient(krakUiApiKey.api_key,krakUiApiKey.api_secret);
 
-window.test = "123";
 
-window.KrakUIApp = window.angular.module('KrakUIApp', []);
-
-window.KrakUIApp.controller('KrakUIController', [ '$scope', function($scope) {
-	$scope.greeting = 'Hola!';
-	$scope.activeTicker = "select ticker";
-	$scope.tickers = ["123","abc","xxx"];
-	$scope.setActiveTicker = function(newActiveTicker) {
-		console.debug("Setting active ticker to : " + newActiveTicker)
-		$scope.activeTicker = newActiveTicker;
-	};
-} ]);
