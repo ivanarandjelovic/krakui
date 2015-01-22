b311561d9323105b2230ff33d979a44373f721ce
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2015-01-22 10:44:35 +0100

    Configuration added and initial tests

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 console.log("Krakui starting ...");
 
+var fs = require('fs');
+var krakUiApiKey = JSON.parse(fs.readFileSync('krakui-api-key.json', 'utf8'));
+
+console.log(krakUiApiKey);
+
+var KrakenClient = require('kraken-api');
+var kraken = new KrakenClient(krakUiApiKey.api_key, krakUiApiKey.api_secret);
+
+/*// Display user's balance
+kraken.api('Balance', null, function(error, data) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(data.result);
+    }
+});*/
+
+// Get Ticker Info
+kraken.api('Ticker', {"pair": 'XXBTZEUR'}, function(error, data) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(data.result);
+    }
+});
 
 
+/*kraken.api('AssetPairs', {}, function(error, data) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(data.result);
+    }
+});*/
 
-
-
-
-
-console.log("Krakui exiting.");
