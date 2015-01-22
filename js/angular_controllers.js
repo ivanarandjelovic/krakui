2b338855f43e0eaf7961c5cbad5deeb4f162899b
aivan <ivan.arandjelovic@gmail.com>
2015-01-22 22:01:39 +0100

    AssetPair selection filled in

--- js/angular_controllers.js
+++ js/angular_controllers.js
@@ -1,25 +1,37 @@
 var KrakUIApp = angular.module('KrakUIApp', []);
 
 KrakUIApp.controller('KrakUIController', [ '$scope', function($scope) {
-	
+
 	$scope.greeting = 'Hola!';
 	$scope.activeTicker = "select ticker";
-	$scope.tickers = [];
-	
+	$scope.tickers = ["none"];
+	$scope.assetPairs = null;
+
 	// Set chosen asset pair
 	$scope.setActiveTicker = function(newActiveTicker) {
 		console.debug("Setting active ticker to : " + newActiveTicker)
 		$scope.activeTicker = newActiveTicker;
 	};
-	
+
 	// get initial set of tradable asset pairs
 	window.KrakUI.kraken.api('AssetPairs', {}, function(error, data) {
-	    if(error) {
-	        console.log(error);
-	    }
-	    else {
-	        console.log(data.result);
-	        $scope.tickers = data.result;
-	    }
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("got asset pairs:")
+			console.log(data.result);
+			$scope.assetPairs = data.result;
+			$scope.tickers = [];
+			for (assetPairName in $scope.assetPairs) {
+				if ($scope.assetPairs.hasOwnProperty(assetPairName)) {
+					$scope.tickers.push(assetPairName);
+				}
+			}
+			if($scope.assetPairs.XXBTZEUR) {
+				$scope.activeTicker = 'XXBTZEUR';
+			}
+			$scope.$apply();
+
+		}
 	});
 } ]);
